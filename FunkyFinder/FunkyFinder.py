7f8084adea3ea057da1d9fc431c7e0acedc952bb
BeginnerCoder3 <skaterfree3@gmail.com>
2026-08-03 21:45:18 -0400

    Still only outputs red but fully functional

--- FunkyFinder/FunkyFinder.py
+++ FunkyFinder/FunkyFinder.py
@@ -1,8 +1,10 @@
 from rdkit import Chem
 from rdkit.Chem import Draw
+import itertools
 
-mol_smiles = 'NC(C(=O)O)C1CC(=O)N1CC(CC(=O)O)C2CC(=O)N2CC(=O)O'
+mol_smiles = 'NCC(N)COCC(=O)NC1CC(=O)N1Cc2c(C(=O)O)c(C(=O)O)c(C(=O)O)c(C(=O)O)c2O'
 mol_obj = Chem.MolFromSmiles(mol_smiles)
+palette = [(1, 0, 0), (1, 0.541, 0), (1, 0.984, 0), (0, 1, 0.071), (0, 0.384, 1), (0.604, 0, 1)] #roygb
 
 funky_dict = {
     "beta-lactam" : "N1C(=O)CC1",
@@ -18,7 +20,12 @@ compiled_patterns = {name: Chem.MolFromSmarts(s) for name, s in funky_dict.items
 
 def find_groups(mol, dict):
     # Directly iterate through key-value pairs (no index tracking needed)
-    matches_highlighted = []
+    atoms_highlighted = {}
+    bonds_highlighted = {}
+    group_colors = {}
+    for (name, obj), color in zip(dict.items(), palette):
+        group_colors[name] = color
+    print(group_colors)
     for name, obj in dict.items():
         if obj is None:
             print(f"Skipping {name}: Invalid SMARTS pattern")
@@ -27,14 +34,30 @@ def find_groups(mol, dict):
         # Check if the molecule matches the compiled pattern
         if mol.HasSubstructMatch(obj):
             matches = mol.GetSubstructMatches(obj)
-            matches_highlighted.extend([num for sub_tuple in matches for num in sub_tuple])
+    
+            for sub_tuple in matches:
+                for atom_idx in sub_tuple:
+                    atoms_highlighted[atom_idx] = group_colors[name]
+    
+            for match in matches:
+                for a_1, a_2 in itertools.combinations(match, 2):
+                    bond = mol.GetBondBetweenAtoms(a_1, a_2)
+                    if bond is not None:
+                        bonds_highlighted[bond.GetIdx()] = group_colors[name]
+    
             num_matches = len(matches)
             if num_matches == 1:
                 print(f"Contains {num_matches} {name} group")
             else:
                 print(f"Contains {num_matches} {name} groups")
-
-    img = Draw.MolToImage(mol, highlightAtoms = matches_highlighted)
+            #print(name, "atoms:", [idx for sub_tuple in matches for idx in sub_tuple])
+    img = Draw.MolToImage(
+    mol,
+    highlightAtoms=list(atoms_highlighted.keys()),
+    highlightBonds=list(bonds_highlighted.keys()),
+    highlightAtomColors=atoms_highlighted,
+    highlightBondColors=bonds_highlighted
+    )    
     img.save('image.png')
 
 # Run the fixed function
